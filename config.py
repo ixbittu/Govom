4c1a6a068a6b5f4b5f21c1f48ad15d5b8d79acb7
ixbittu <shivamlkr628@gmail.com>
2026-01-07 16:19:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ixbittu/SaregamaMusic",
+    "https://github.com/ixbittu/Govom",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
